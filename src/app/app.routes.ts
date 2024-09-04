292f1a2136567d0fe77eb5023cfaab44912a0c1c
mateus-lopes <mateusalbano22@gmail.com>
2024-09-04 16:12:16 -0300

    feat: :art: creating scope

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { MyServicesComponent } from './views/my-services/my-services.component';
+import { ServicesAddComponent } from './views/services-add/services-add.component';
+import { ServicesPanelComponent } from './views/services-panel/services-panel.component';
+import { ServicesReportsComponent } from './views/services-reports/services-reports.component';
+import { NotFoundComponent } from './views/not-found/not-found.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
+    },
+    {
+        path: 'my-services',
+        component: MyServicesComponent
+    },
+    {
+        path: 'services-add',
+        component: ServicesAddComponent
+    },
+    {
+        path: 'services-panel',
+        component: ServicesPanelComponent
+    },
+    {
+        path: 'services-reports',
+        component: ServicesReportsComponent
+    },
+    {
+        path: 'not-found',
+        component: NotFoundComponent
+    },
+    {
+        path: '**',
+        redirectTo: '/not-found'
+    },
+];
