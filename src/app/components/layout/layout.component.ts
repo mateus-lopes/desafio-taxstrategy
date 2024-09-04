a3949a6c4f87251463cc2eb76a719e388fe7d76f
mateus-lopes <mateusalbano22@gmail.com>
2024-09-04 16:58:52 -0300

    feat: :sparkles: updating layout grid

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { LogoComponent } from "../logo/logo.component";
 import { NavbarComponent } from "../navbar/navbar.component";
+import { TitlePageComponent } from "../title-page/title-page.component";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   imports: [
-    LogoComponent, 
-    NavbarComponent
-  ],
+    NavbarComponent,
+    TitlePageComponent
+],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,15 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 import { NavComponent } from "../nav/nav.component";
+import { LogoComponent } from "../logo/logo.component";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
     NgOptimizedImage,
-    NavComponent
+    NavComponent,
+    LogoComponent
 ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
