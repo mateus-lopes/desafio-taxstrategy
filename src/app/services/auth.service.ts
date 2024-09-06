c180a334dbcabbe0438cef58fa567fa2802fe632
mateus-lopes <mateusalbano22@gmail.com>
2024-09-06 13:51:43 -0300

    fix: :bug: Update styles.scss and service-table.component.html

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,10 +5,8 @@ import { UserService } from './user.service';
   providedIn: 'root'
 })
 export class AuthService {
-
   constructor(public userService: UserService) {   }
 
-  // Método para login
   login(username: string, password: string): boolean {
     if (username != '' && password != '') {
       const user = this.userService.getUser(username);
@@ -19,17 +17,14 @@ export class AuthService {
     return false;
   }
 
-  // Método para logout
   logout(): void {
     localStorage.removeItem('currentUser');
   }
 
-  // Verifica se o usuário está logado
   isLoggedIn(): boolean {
     return localStorage.getItem('currentUser') !== null;
   }
 
-  // Obtém o usuário atual
   getCurrentUser(): any {
     return JSON.parse(localStorage.getItem('currentUser') || '{}');
   }
